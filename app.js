c62081a0cab7685f0df29addd5b9439f834546e0
windyoung1990 <1078156801@qq.com>
2022-07-30 20:00:22 +0800

    history

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const history = require('connect-history-api-fallback');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const app = express();
-app.use(express.static('static'));
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 // app.get('/', function (req, res) {
 //     res.render(path.join(__dirname +  '/index.html'))
 //   })
 // app.get('/', (req, res) => {
 //     res.render(path.join(__dirname +  '/index.html'))
 // });
-// app.use(history({
-//     index: '/index.html'
-// }));
-// app.use(express.static('dist'));
+app.use(history({
+    index: '/lierjie/index.html'
+}));
+app.use(express.static('dist'));
+app.use(express.static('static'));
 
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname +  '/index.html'))
+});
 
 app.use(bodyParser.json({
     limit: '10MB'
