5a61c4002ac635ae6832b7163eee7186f1b1f594
windyoung1990 <1078156801@qq.com>
2022-07-30 17:30:23 +0800

    初始化李二姐项目

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const app = express();
+
+app.use(history({
+    index: '/index.html'
+}));
+app.use(express.static('dist'));
+app.use(express.static('static'));
+
+app.use(bodyParser.json({
+    limit: '10MB'
+}))
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+// 启动gzip
+app.use(compression());
+
+http.createServer(app).listen(6688);
